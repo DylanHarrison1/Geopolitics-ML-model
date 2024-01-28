a2a6cd4c66a9432221218012227870649b804068
DylanHarrison1 <dylanchristopherharrison@outlook.com>
2024-01-28 14:17:48 +0000

    Basic NN model

--- model.py
+++ model.py
@@ -1,1 +1,23 @@
+import torch
+from torch import nn
+import numpy as np
 
+class Model(torch.nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super(Model, self).__init__()
+
+        layers = [
+            nn.Linear(inputDims, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 32)
+        ]
+
+        self.NN = torch.nn.Sequential(*layers)
+        
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.003)
+
+       
