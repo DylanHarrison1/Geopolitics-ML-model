1f23a231d43b96ff67f7e6c13c367ea3077a1c30
DylanHarrison1 <dylanchristopherharrison@outlook.com>
2024-02-04 22:42:22 +0000

    Learnt panda

--- preprocessing.py
+++ preprocessing.py
@@ -1,18 +1,27 @@
-import csv
-
+import pandas as pd
+import os
 #Goals:
+# - 
 # - Create A HDI file per country per year
 # - Simplify 
 # - 
 # - 
 # - 
 
+def FetchData(row, collumn, directory):
+    curDir = os.getcwd()
+    df = pd.read_csv(curDir + directory)
+    print(df.iloc[(0,3)])
+
+
 def CreateHDIFile():
-    with open('data\raw\AHDI (1870-2020) (excl income).csv', newline='') as csvfile:
-        dataReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in dataReader:
-            print(', '.join(row))
+    #df = pd.read_csv('..\data\raw\AHDI (1870-2020) (excl income).csv')
+    #while (True):
 
-
+    #    df.insert()
+    pass
 def Simplify_xxxx_File():
     pass
+
+
+FetchData(1, None, '\data\\raw\AHDI (1870-2020) (excl income).csv')
